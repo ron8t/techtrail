3e72518d2eb5d630c3afbee4e79cb5990418442f
ron <richard.nofori@gmail.com>
2024-03-14 15:58:06 -0700

    all in

---
+++ src/main/java/com/rontech/tech/trial/TechTrialApplication.java
@@ -0,0 +1,13 @@
+package com.rontech.tech.trial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTrialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rontech/tech/trial/TechTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rontech.tech.trial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
